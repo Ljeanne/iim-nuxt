8830f8337e9cd157a7b00cadb0c3f60b3d8e2dac
Ljeanne <ludovic.jn@gmail.com>
2019-05-13 10:54:45 +0200

    yolo

---
+++ pages/blog/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="container">
+  <h2>{{title}}</h2>
+  <p>{{currentArticles.title}} </p>
+  <ul>
+    <li v-for="article in articles" :key="article.id">
+        {{ article.title }}</li>
+    </ul>
+  </section>
+</template>
+<script>
+export default{
+
+    data(){
+        return{
+            currentArticles: null,
+            //title: "Mes articles",
+            articles: [
+                {id: 1, title: 'Articles1', content: '...'},
+                {id: 2, title: 'Articles2', content: '...'},
+                {id: 3, title: 'Articles3', content: '...'},
+                {id: 4, title: 'Articles4', content: '...'},
+            ]
+        }
+    },
+    mounted(){
+
+        this.currentArticles = this.articles.find(item => item.id === this.$route.params.id);
+    }
+}
+</script>
